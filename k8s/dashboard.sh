71d5742b2883d141b901cf82577d298a3b64c682
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2025-09-03 14:27:55 +0200

    Add port-forwarding command for Kubernetes Dashboard service

--- k8s/dashboard.sh
+++ k8s/dashboard.sh
@@ -2,3 +2,5 @@
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
